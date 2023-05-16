40e066fe45187538ee47e5a154b0f76b8ad5365b
ZH-Himel <zh.himel.2002@gmail.com>
2023-05-16 13:34:03 +0600

    icon fix

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 // for aside 
 console.log("Welcome");
 suraindex=0;
-let audioplay=new Audio("Surah/1.mp3");
-let masterplay=document.querySelector("#masterplay");
+var audioplay=new Audio("Surah/1.mp3");
+const masterplay=document.querySelector("#masterplay");
 let progressbar=document.querySelector("#progressbar");
 let allsurah=document.querySelectorAll(".suraicon");
+
 masterplay.addEventListener("click",(e)=>{
-    console.log(e.target.id);
-    if(audioplay.paused||audioplay.currentTime==0){
+    console.log(audioplay.target.id);
+    if(audioplay.currentTime==0|| audioplay.paused){
         e.target.classList.remove("fa-play-circle");
         e.target.classList.add("fa-pause-circle");
         audioplay.play();
     }
     else{
+
         e.target.classList.remove("fa-pause-circle");
-        e.target.classList.add("fa-play-circle");
+        e.target.classList.add("fa-play-circle");  
         audioplay.pause();
     }
 })
-
 // middlesection
 function playallicon(){
     Array.from(allsurah).forEach((element)=>{
@@ -32,6 +33,8 @@ function playfunction(e){
     playallicon();
     e.target.classList.remove("fa-play-circle");
     e.target.classList.add("fa-pause-circle");
+    masterplay.classList.remove("fa-play-circle");
+    masterplay.classList.add("fa-pause-circle");
     audioplay.play();
 }
 function pausefunction(e){
@@ -41,11 +44,12 @@ function pausefunction(e){
 }
 Array.from(allsurah).forEach((element)=>{
     element.addEventListener("click",function(e){
-        console.log(audioplay.currentTime);
         if(audioplay.currentTime==0||audioplay.paused){
             playfunction(e);
         }
         else{
+            masterplay.classList.remove("fa-pause-circle");
+            masterplay.classList.add("fa-play-circle");
             pausefunction(e);
             if(audioplay.src!==`http://127.0.0.1:5500/Surah/${e.target.id}.mp3`){
                 console.log(audioplay.src);
@@ -54,3 +58,4 @@ Array.from(allsurah).forEach((element)=>{
         }
     })
 })
+
